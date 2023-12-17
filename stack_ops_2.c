6965d6a83b89036ff83949452d7ac85276547e18
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-16 21:38:23 -0800

    issue fixing

--- stack_ops_2.c
+++ stack_ops_2.c
@@ -52,7 +52,7 @@ void sub(stack_t **stack, unsigned int line_num)
         }
 
         temp = arg_s->top->next;
-        temp->n = arg_s->top->n + temp->n;
+        temp->n = temp->n - arg_s->top->n;
         remove_node();
         arg_s->stack_len -= 1;
 }
