a4c238e7329225adf1d90a8446ff44f0f2d5f6d5
kankeuNde <rodriguekankeu1@gmail.com>
2023-12-16 21:29:46 -0800

    sub opcode implementation

--- opcode_extraction.c
+++ opcode_extraction.c
@@ -15,6 +15,7 @@ void get_opcode(void)
 		{"swap", &swap},
 		{"add", &add},
 		{"nop", &nop},
+		{"sub", &sub},
 		{NULL, NULL}
 	};
 	i = 0;
--- stack_ops_2.c
+++ stack_ops_2.c
@@ -33,3 +33,26 @@ void nop(stack_t **stack, unsigned int line_num)
 	(void) stack;
 	(void) line_num;
 }
+
+/**
+ * sub - substractsp the top two elts
+ * @stack: Pointer to the stack
+ * @line_num: Current line number
+ */
+void sub(stack_t **stack, unsigned int line_num)
+{
+        stack_t *temp;
+
+        (void) stack;
+        if (arg_s->stack_len < 2)
+        {
+                dprintf(2, "L%d: can't sub, stack too short\n", line_num);
+                free_all();
+                exit(EXIT_FAILURE);
+        }
+
+        temp = arg_s->top->next;
+        arg_s->top->n = arg_s->top->n + temp->n;
+        remove_node();
+        arg_s->stack_len -= 1;
+}
